b6b7a67a7609df248bc8ded61c45b2f5b5a4f2e9
Max Schmitt <max@schmitt.mx>
2020-03-21 03:46:29 +0100

    fix: import

--- shoppingline/api/views.py
+++ shoppingline/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import generics, status
 from django.contrib.auth import login, logout
+from django.views.decorators.csrf import csrf_exempt
 
 from shoppingline.users.models import User
 from shoppingline.api import serializers
